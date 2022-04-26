2af9d290d14d4886b4c814e583e822377ccda273
Ren Wang <iamwrm@gmail.com>
2022-04-26 04:56:09 +0000

    copy images

---
+++ scripts/copy_images.sh
@@ -0,0 +1,2 @@
+skopeo copy docker://registry.redhat.io/rhscl/devtoolset-11-toolchain-rhel7:latest docker://ghcr.io/ren15/devtoolset-11-toolchain-rhel7:latest
+skopeo copy docker://registry.redhat.io/ubi8/ubi docker://ghcr.io/ren15/ubi8/ubi
