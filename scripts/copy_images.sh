71437c4ac3782883fd5f9eb9082457322a529b3c
Ren Wang <iamwrm@gmail.com>
2022-04-26 05:00:10 +0000

    add perftools

--- scripts/copy_images.sh
+++ scripts/copy_images.sh
@@ -1,2 +1,3 @@
 skopeo copy docker://registry.redhat.io/rhscl/devtoolset-11-toolchain-rhel7:latest docker://ghcr.io/ren15/devtoolset-11-toolchain-rhel7:latest
+skopeo copy docker://registry.redhat.io/rhscl/devtoolset-11-perftools-rhel7:latest docker://ghcr.io/ren15/devtoolset-11-perftools-rhel7:latest
 skopeo copy docker://registry.redhat.io/ubi8/ubi docker://ghcr.io/ren15/ubi8/ubi
