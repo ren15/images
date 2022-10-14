c18a0d8e99dbc524b6cae1ee7cdd541833f8fd22
iamwrm <iamwrm@gmail.com>
2022-10-14 23:28:09 +0800

    use core

--- docker_build/ubuntu_kasm/pre.sh
+++ docker_build/ubuntu_kasm/pre.sh
@@ -1,3 +1,9 @@
 set -xe 
 
 echo "ubuntu_kasm needs more"
+
+
+git clone https://github.com/kasmtech/workspaces-images docker_build/ubuntu_kasm/workspaces-images
+mv docker_build/ubuntu_kasm/workspaces-images/src docker_build/ubuntu_kasm/src
+
+
